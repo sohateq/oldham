8f4c9c6f65b9b331441f678d04b6becb92020ac7
sohateq <kameko2008@yandex.ru>
2018-07-12 18:38:38 +0300

    Добавлен экран оформления заказа. Осталось сделать кнопку для него.

---
+++ app/src/main/java/com/example/a1/myapplication/order/OrderActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1.myapplication.order;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
